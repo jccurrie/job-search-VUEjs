26556bc83eae553c1afa83abb6fdca43ad41e0cd
Julian Currie <jccurrie@uab.edu>
2023-07-16 16:35:13 -0500

    More vitest setup, component testing setup, MainNavTesting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # job-search-VUEjs
+
+### Setup Instructions
+
+Execute npm install to download NPM dependencies.
+
+Execute npm run dev to launch the Vue app in your browser.
---
+++ job-search/tests/unit/components/MainNav.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/vue';
+
+import MainNav from '@/components/MainNav.vue';
+
+describe('MainNav', () => {
+  it('displays company name', () => {
+    render(MainNav);
+    const companyName = screen.getByText('JCurrie Careers');
+    expect(companyName).toBeInTheDocument();
+  });
+});
